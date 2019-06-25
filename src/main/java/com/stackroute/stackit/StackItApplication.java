5fedfa26a460803daa28d71850c55b7a10448b15
Somsubhra Mukherjee <somsubhramukherjee@Somsubhras-Official-MacBook-Air.local>
2019-06-25 16:32:14 +0530

    Initial commit

---
+++ src/main/java/com/stackroute/stackit/StackItApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.stackit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackItApplication.class, args);
+	}
+
+}
